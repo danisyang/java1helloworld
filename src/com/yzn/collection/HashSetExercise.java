3634a5396ac69808920d17249706a61fd3eaa06a
Dennis Yang <yzn311976@gmail.com>
2017-02-22 23:39:17 +0800

    2/21.snow

---
+++ src/com/yzn/collection/HashSetExercise.java
@@ -0,0 +1,18 @@
+package com.yzn.collection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Dennis Yang on 2017/2/22.
+ */
+public class HashSetExercise {
+    public static void main(String[] args){
+        Set set = new HashSet();
+        set.add("3");
+        set.add("1");
+        set.add("2");
+        set.add("1");
+        System.out.println(set);
+    }
+}
